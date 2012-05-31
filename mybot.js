a877d4b77f1bc3da6a6a6dd572c96948256c3aec
Jobaer <jobaer.du@gmail.com>
2012-05-31 12:31:35 +0600

    Added a function for getting all the available items. Also added a Point type for convinience.

--- mybot.js
+++ mybot.js
@@ -3,7 +3,7 @@ function new_game() {
 
 function make_move() {
    var board = get_board();
-
+   
    // we found an item! take it!
    if (board[get_my_x()][get_my_y()] > 0) {
        return TAKE;
@@ -18,3 +18,27 @@ function make_move() {
 
    return PASS;
 }
+
+
+function get_available_items(){
+  var available_items = new Array();
+  
+  var board = get_board();  
+  for(var i=0; i<WIDTH; i++){
+    for(var j=0; j<HEIGHT; j++) {
+	  var item_type = board[i][j];
+	  if(item_type>0){
+	   available_items.push(new Point(i, j, item_type));
+	  }
+	}
+  }
+  
+  return available_items;
+}
+
+function Point(x,y,item_type){
+  this.x = x;
+  this.y = y;
+  this.item_type = item_type;
+}
+
