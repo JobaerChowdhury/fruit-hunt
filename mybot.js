f6d76d8c981a257d1e5506c9ed16eca2f546b439
Jobaer <jobaer.du@gmail.com>
2012-06-01 14:17:20 +0600

    Added a method for computing available and owned items.

--- mybot.js
+++ mybot.js
@@ -1,8 +1,11 @@
 function new_game() {
+
 }
 
 function make_move() {
    var board = get_board();
+   
+   next_best();
    
    // we found an item! take it!
    if (board[get_my_x()][get_my_y()] > 0) {
@@ -19,13 +22,40 @@ function make_move() {
    return PASS;
 }
 
+// maybe sort all the items and give them some priority. then go after one by one. at the 
+// begining of each move re-calculate the priorities.
 
 function update_strategy(){
   // will be called each time before taking next step. 
 }
 
 function next_best(){
-  // return the next best item to go for 
+  var available_map = get_available_total(); 
+
+  // strategy --- which item has minimum numbers.   
+  
+  /*  is it worth to pursue that type of item?  -- pursue if you are closer or same distance than opponent.
+       which is the closest?
+	     is the opponent closer than me? 
+		   go for that item.
+		   
+  */
+}
+
+
+/*
+ Returns a map - containing the number of items available for picking or owned. This map will be used 
+ to determine which item-type we should target next.
+*/
+function get_available_total(){
+   var number_it = get_number_of_item_types();
+   
+   var available_total = {};
+   for(var i=1; i<= number_it; i++) {
+     var available = get_total_item_count(i) - get_opponent_item_count(i);
+	 available_total[i] = available;
+   }
+   return available_total;
 }
 
 function get_available_items(){
@@ -50,3 +80,13 @@ function Item(item_type, x, y){
   this.y = y;
 }
 
+
+
+/* 
+ Assumptions - 
+ It seems if the number of item types is 4 then the specific types will be 1,2,3,4. 
+
+*/
+
+// find some suitable data structures. 
+// Node, priority
